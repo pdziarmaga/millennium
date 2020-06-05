25303d5b5bd7e9315495bf8e4694e004ec315bc9
dziara <p.dziarmaga@viro.fit>
2020-06-05 18:57:58 +0200

    Adding domain

---
+++ millennium/Millennium.Domain/User.cs
@@ -0,0 +1,31 @@
+﻿using Millennium.Shared.BaseObjects;
+
+namespace Millennium.Domain
+{
+    public class User : Entity
+    {
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+
+        public User(
+            string name,
+            string surname)
+        {
+            Name = name;
+            Surname = surname;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} {Surname}";
+        }
+
+        public void Update(
+            string name,
+            string surname)
+        {
+            Name = name;
+            Surname = surname;
+        }
+    }
+}
